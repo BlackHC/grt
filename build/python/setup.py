20ad4995a3e718b21a78c77926f6fa9d26c5cfd3
Andreas Kirsch <blackhc@gmail.com>
2022-11-28 23:12:58 +0000

    Fixes to make setup.py run
    It cannot execute in isolation because it cannot install libgrt (ofc). Maybe make this conda-only somehow?

--- build/python/setup.py
+++ build/python/setup.py
@@ -1,18 +1,25 @@
 # Setup tools for the SWIG GRT python bindings
 from distutils.core import setup, Extension
-import numpy as np
 import sys
 
-include_dirs = [np.get_include(), sys.prefix + "/include"]
 
-# Assume libgrt has been installed as a conda package
-# and it is available in the conda environment.
+class get_numpy_include(object):
+    """Defer numpy.get_include() until after numpy is installed."""
+
+    def __str__(self):
+        import numpy
+        return numpy.get_include()
+
+
+include_dirs = [sys.prefix + "/include"]
+
+# Assume libgrt has been installed as a conda package, and that it is available in the conda environment.
 # Create an extension module for the swig wrapper
 grt_module = Extension(
     "_GRT",
     sources=["GRT.i"],
     swig_opts=["-python", "-py3", "-c++", "-Wall", "-verbose"] + ["-I" + d for d in include_dirs],
-    include_dirs=include_dirs,
+    include_dirs=include_dirs + [get_numpy_include()],
     library_dirs=[sys.prefix + "/lib"],
     libraries=["grt"],
 )
@@ -20,11 +27,11 @@ grt_module = Extension(
 
 # Create the setup
 setup(
-    name="GRT",
-    version="0.2.5",
+    name="blackhc.GRT",
+    version="0.2.5.dev0",
     author="Nick Gillian",
     description="""Python bindings for the Gesture Recognition Toolkit (GRT)""",
     ext_modules=[grt_module],
     py_modules=["GRT"],
-    requires=["numpy", "swig"],
+    install_requires=["numpy", "swig", "libgrt"],
 )
