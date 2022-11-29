26eb7d86731dedfa25f7d64b9a03b7cde3773514
blackhc <blackhc@gmail.com>
2022-11-29 15:29:13 +0000

    Fixes for Apple builds
    Tested on ARM64

--- build/python/setup.py
+++ build/python/setup.py
@@ -13,6 +13,10 @@ class get_numpy_include(object):
 
 include_dirs = [sys.prefix + "/include"]
 
+# Different extra compiler options for different platforms?
+extra_compile_args = ["-std=c++11"]
+extra_link_args = []
+
 # Assume libgrt has been installed as a conda package, and that it is available in the conda environment.
 # Create an extension module for the swig wrapper
 grt_module = Extension(
@@ -22,16 +26,18 @@ grt_module = Extension(
     include_dirs=include_dirs + [get_numpy_include()],
     library_dirs=[sys.prefix + "/lib"],
     libraries=["grt"],
+    extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
 )
 
 
 # Create the setup
 setup(
     name="blackhc.GRT",
-    version="0.2.5.dev0",
+    version="0.2.5.dev1",
     author="Nick Gillian",
     description="""Python bindings for the Gesture Recognition Toolkit (GRT)""",
     ext_modules=[grt_module],
     py_modules=["GRT"],
-    install_requires=["numpy", "swig", "libgrt"],
+    install_requires=["numpy", "swig"],
 )
